9090da6a1ba7f8aa5e0eca12cc4a211859d1c9b4
Brice Sanchez <contact@brice-sanchez.com>
2018-06-13 00:16:49 -0400

    WIP: image crop

---
+++ core/app/assets/javascripts/refinery/image_crop.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  var image = document.getElementById('crop');
+
+  console.log(image);
+  var cropper = new Cropper(image, {
+    aspectRatio: 16 / 9,
+    crop: function(event) {
+      console.log(event.detail.x);
+      console.log(event.detail.y);
+      console.log(event.detail.width);
+      console.log(event.detail.height);
+      console.log(event.detail.rotate);
+      console.log(event.detail.scaleX);
+      console.log(event.detail.scaleY);
+    }
+  });
+});
